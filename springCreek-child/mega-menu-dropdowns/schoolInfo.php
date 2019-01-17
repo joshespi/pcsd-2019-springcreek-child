b8b229e8c802fae00c3e802013b965dfe784b0b7
Josh Espinoza <joshespi@gmail.com>
2019-01-17 09:15:30 -0700

    Initial Upload

---
+++ springCreek-child/mega-menu-dropdowns/schoolInfo.php
@@ -0,0 +1,41 @@
+<div class="dropDownColumn">
+	<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/calendars.svg" alt="" />Calendars</h3>
+		<ul>
+			<li class="int"><a href="https://springcreek.provo.edu/all-events-calendar/">All Events Calendar</a></li>
+			<li class="int"><a href="https://springcreek.provo.edu/calendar/">Spring Creek Calendar</a></li>
+			<li class="int"><a href="https://springcreek.provo.edu/district-school-year-calendar/">School Year Calendar</a></li>
+		</ul>
+	<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/parents.svg" alt="" />Parent Resources</h3>
+		<ul>
+			<li class="int"><a href="https://springcreek.provo.edu/study-at-home/">Study At Home</a></li>
+			<li class="ext"><a href="https://grades.provo.edu/public/">Grades & Attendance (PowerSchool)</a></li>
+			<li class="int"><a href="https://springcreek.provo.edu/faculty-staff/teachers-directory/">Classrooms</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />About Spring Creek</h3>
+		<ul>
+			<li class="int"><a href="https://springcreek.provo.edu/school-information/contact-our-school/">Contact Our School</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/school-hours/">Hours of Operation &amp; Daily Schedule</a></li>
+            <li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/Spring-Creek-Achievment-Data.pdf">Achievement Data</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/community-council/">Community Council</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/after-school-programs/">After School Programs</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/school-profile/">School Profile</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/parent-teacher-association/">Parent Teacher Association</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/behavior-expectations/">Behavior Expectations</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/school-information/drop-off-pick-up-procedures/">Drop Off/Pick Up Procedures</a></li>
+            <li class="int"><a href="https://provo.edu/transportation/find-your-bus-route/">Bus Routes</a></li>
+            <li class="int"><a href="https://springcreek.provo.edu/category/school-newsletter/">School Newsletter</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
+		<ul>
+			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
+            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
+            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
+		</ul>
+	
+</div>
---
+++ springCreek-child/mega-menu-dropdowns/titlei.php
@@ -0,0 +1,34 @@
+				<div class="dropDownColumn">
+					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+						<ul>
+							<li class="int"><a href="https://springcreek.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/2016-17-Pamphlet-Annual-Title-I-Information-for-Parents.pdf">Annual Title I Information for Parents</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/SC-of-FY18-19-Annual-TI-Mtg-.pdf">Annual Title I Presentation</a></li>
+						</ul>
+						
+				</div>
+				<div class="dropDownColumn">
+					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Utah State Office of Education School Reports</h3>
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/PACE-School-Report-Card.pdf">PACE Report</a></li>
+							
+						</ul>
+					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Learning Compact</h3>
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/2018-19Parent-Compact-Englishcolor.pdf">Parent-School Learning Compact (English)</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/2018-19Parent-Compact-Spanish-color.pdf">Parent-School Learning Compact (Spanish)</a></li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Involvement Policy</h3>
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/Parent-Involvement-Policy-2016-17-flyer-1.pdf">Parent Involvement Policy (English)</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/Parent-Involvement-Policy-flyer-Spanish.pdf">Parent Involvement Policy (Spanish)</a></li>
+						</ul>
+					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Additional Resources</h3>
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/School-Improvement-Plan.pdf">School Improvement Plan</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/2018-19-Title-I-Goals-.pdf">Title I Goals</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/Parent-Right-to-Know-EngSpan-.pdf">Parent Right to Know</a></li>
+						</ul>
+				</div>
