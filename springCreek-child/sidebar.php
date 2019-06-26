20395dd067fee962449d705471e4f8f8ea5f143b
Josh Espinoza <joshespi@gmial.com>
2019-06-26 15:36:25 -0600

    06-26-19 updates

--- springCreek-child/sidebar.php
+++ springCreek-child/sidebar.php
@@ -98,6 +98,7 @@
 					</li>
 					
 				</ul>
+				<a href="https://www.peachjar.com/index.php?region=33078&a=28&b=138"><img src="https://springcreek.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
 				<a href="https://healthcare.utah.edu/uni/safe-ut/"><img src="https://springcreek.provo.edu/wp-content/uploads/2019/03/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
 			</section>
 		</aside>
