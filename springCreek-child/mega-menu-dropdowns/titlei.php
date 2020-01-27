5c170b26848e080fcc97c8af7c76d0db3f6c0712
Josh Espinoza <joshespi@gmial.com>
2020-01-27 09:20:43 -0700

    Updated School Report Card

--- springCreek-child/mega-menu-dropdowns/titlei.php
+++ springCreek-child/mega-menu-dropdowns/titlei.php
@@ -8,26 +8,24 @@
 						<p>Title I Coordinator: Lucy Quintero (EvaluceQ@provo.edu)</p>
 				</div>
 				<div class="dropDownColumn">
-					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Utah State Office of Education School Reports</h3>
-						<ul>
-							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/10282019-school_report_card.pdf">School Report Card</a></li>
-							
-						</ul>
+					
 					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Learning Compact</h3>
 						<ul>
 							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/10282019-Parent-School-Learning-Compact-English.pdf">Parent-School Learning Compact (English)</a></li>
 							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/10282019-Parent-School-Learning-Compact-Spanish.pdf">Parent-School Learning Compact (Spanish)</a></li>
 						</ul>
-				</div>
-				<div class="dropDownColumn">
 					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Engagement Policy</h3>
 						<ul>
 							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/10282019-fy20-Parent-Engagement-Policy-English.pdf">Parent Engagement Policy (English)</a></li>
 							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/10282019-fy20-Parent-Engagement-Policy-Spanish.pdf">Parent Engagement Policy (Spanish)</a></li>
 						</ul>
+				</div>
+				<div class="dropDownColumn">
+					
 					<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Additional Resources</h3>
 						<ul>
 							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/Parent-Right-to-Know-EngSpan-.pdf">Parent Right to Know</a></li>
 							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/SC_AM_November_FY20_TI-Annual-Meeting-slides.pdf">Annual Title I Meeting Info</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2020/01/SC_SchoolReportCard_fy19.pdf">School Report Card</a></li>
 						</ul>
 				</div>
